bb7b09de52e7d7f1322599cce0cbcf03e7a3b083
João Carlos Rebouças <70778525+jcr04@users.noreply.github.com>
2023-06-12 19:46:13 -0300

    adjusting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
         {/* Adicione outras rotas aqui, como a página de detalhes do produto, carrinho, etc. */}
       </Switch>
-    </Router>
+    </BrowserRouter>
   );
 };
 
