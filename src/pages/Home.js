096d229527add0c574ee53f5ec4ec57a34f1977e
João Carlos Rebouças <70778525+jcr04@users.noreply.github.com>
2023-06-14 19:54:46 -0300

    add more products

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,20 @@ import axios from 'axios';
 import ProductList from '../components/ProductList';
 
 const Home = ({ handleAddToCart }) => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([
+    { id: 1, name: "Água Mineral 500ml", price: 2.5, image: "url-da-imagem" },
+    { id: 2, name: "Água Mineral 1L", price: 4.5, image: "url-da-imagem" },
+    { id: 3, name: "Água Mineral 5L", price: 10.0, image: "url-da-imagem" },
+    { id: 4, name: "Água Mineral com gás 500mL", price: 3.0, image: "url-da-imagem" },
+    { id: 5, name: "Água Mineral com gás 1L", price: 5.0, image: "url-da-imagem" },
+    { id: 6, name: "Água Mineral com gás 2L", price: 7.0, image: "url-da-imagem" },
+    { id: 7, name: "Água Mineral com gás 5L", price: 15.0, image: "url-da-imagem" },
+  ]);
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get('https://api.example.com/products');
+        const response = await axios.get('http://localhost:3000/products');
         setProducts(response.data);
       } catch (error) {
         console.log(error);
