73efbbbc45817c4f3e1caf6decdae81303ab5bc5
João Carlos Rebouças <70778525+jcr04@users.noreply.github.com>
2023-06-15 14:12:56 -0300

    add feature

--- src/pages/Colaboradores.js
+++ src/pages/Colaboradores.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+
 const Colaboradores = () => {
   const [formData, setFormData] = useState({
     name: '',
@@ -13,27 +14,30 @@ const Colaboradores = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Aqui você pode enviar os dados do formulário para a API ou fazer o processamento necessário
-    console.log(formData);
-    // Limpar o formulário
-    setFormData({ name: '', email: '', phone: '' });
+
+    if (formData.name && formData.email && formData.phone) {
+      console.log(formData);
+      setFormData({ name: '', email: '', phone: '' });
+    } else {
+      alert('Por favor, preencha todos os campos obrigatórios.');
+    }
   };
 
   return (
-    <div>
+    <div className="form-container">
       <h1>Página de Colaboradores</h1>
       <form onSubmit={handleSubmit}>
         <label>
           Nome:
-          <input type="text" name="name" value={formData.name} onChange={handleChange} />
+          <input type="text" name="name" value={formData.name} onChange={handleChange} required />
         </label>
         <label>
           Email:
-          <input type="email" name="email" value={formData.email} onChange={handleChange} />
+          <input type="email" name="email" value={formData.email} onChange={handleChange} required />
         </label>
         <label>
           Telefone:
-          <input type="text" name="phone" value={formData.phone} onChange={handleChange} />
+          <input type="text" name="phone" value={formData.phone} onChange={handleChange} required />
         </label>
         <button type="submit">Enviar</button>
       </form>
