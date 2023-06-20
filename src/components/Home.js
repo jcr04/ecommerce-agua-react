036c034070623810eed18a46ad44ad93a89e2d89
João Carlos Rebouças <70778525+jcr04@users.noreply.github.com>
2023-06-20 19:45:10 -0300

    Merge pull request #2 from jcr04/feature/swithpage adcionar cheackout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import ProductList from '../pages/ProductList';
+import CartPage from '../pages/CartPage';
 
-const Home = ({ handleAddToCart }) => {
-  const [products, setProducts] = useState([
-    { id: 1, name: "Água Mineral 500ml", price: 2.5, image: "url-da-imagem" },
-    { id: 2, name: "Água Mineral 1L", price: 4.5, image: "url-da-imagem" },
-    { id: 3, name: "Água Mineral 5L", price: 10.0, image: "url-da-imagem" },
-    { id: 4, name: "Água Mineral com gás 500mL", price: 3.0, image: "url-da-imagem" },
-    { id: 5, name: "Água Mineral com gás 1L", price: 5.0, image: "url-da-imagem" },
-    { id: 6, name: "Água Mineral com gás 2L", price: 7.0, image: "url-da-imagem" },
-    { id: 7, name: "Água Mineral com gás 5L", price: 15.0, image: "url-da-imagem" },
-  ]);
+const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
+
+  const handleAddToCart = (product) => {
+    setCartItems([...cartItems, product]);
+  };
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -31,7 +29,10 @@ const Home = ({ handleAddToCart }) => {
       {products.length === 0 ? (
         <p>Carregando produtos...</p>
       ) : (
-        <ProductList products={products} handleAddToCart={handleAddToCart} />
+        <>
+          <ProductList products={products} handleAddToCart={handleAddToCart} />
+          <CartPage cartItems={cartItems} />
+        </>
       )}
     </div>
   );
