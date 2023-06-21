e97a82381324afd3ba87730cf7ebee238cbb4fb8
João Carlos Rebouças <70778525+jcr04@users.noreply.github.com>
2023-06-21 18:20:22 -0300

    adjusting

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ProductList from '../pages/ProductList';
 import CartPage from '../pages/CartPage';
 
