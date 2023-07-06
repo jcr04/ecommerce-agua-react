52328316a35a7e15b6737784fd9d65bdc756d48d
Luís Guilherme <120354142+gui2077@users.noreply.github.com>
2023-07-06 11:35:23 -0300

    Modificações na página de clientes.

--- src/pages/Clientes.js
+++ src/pages/Clientes.js
@@ -24,32 +24,39 @@ const Clients = ({ addCollaborator }) => {
   };
 
   return (
-    <div>
+    <div className='clients-box'>
       <h1>Página de Clientes</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
+      <form onSubmit={handleSubmit} className='form'>
+
+        <label className='label'>
           Nome:
-          <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+          <input className='input' type="text" name="name" value={formData.name} onChange={handleChange} required />
         </label>
-        <label>
+        <br />
+
+        <label className='label'>
           Email:
-          <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+          <input className='input' type="email" name="email" value={formData.email} onChange={handleChange} required />
         </label>
-        <label>
+        <br />
+
+        <label className='label'>
           Telefone:
-          <input type="tel" name="phone" value={formData.phone} onChange={handleChange} required />
+          <input className='input' type="tel" name="phone" value={formData.phone} onChange={handleChange} required />
         </label>
+        <br />
+
         <button type="submit">Adicionar Colaborador</button>
       </form>
 
       {/* Lista de clientes */}
-      <h2>Clientes</h2>
+      <h2 className='clientes-secao-h2'>Clientes</h2>
       <ul>
-        <li>
+        <li className='clientes-perfis'>
           <span>Cliente 1</span>
           <Link to="/profile/1">Ver Perfil</Link>
         </li>
-        <li>
+        <li className='clientes-perfis'>
           <span>Cliente 2</span>
           <Link to="/profile/2">Ver Perfil</Link>
         </li>
