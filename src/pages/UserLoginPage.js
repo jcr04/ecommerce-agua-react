325d69fa8bb35bd5cad75d1d9cb66729edf6d653
jcr04 <joaocarlosreboucas15@gmail.com>
2023-07-12 19:16:30 -0300

    add valiadation e localstorage

--- src/pages/UserLoginPage.js
+++ src/pages/UserLoginPage.js
@@ -1,47 +1,98 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { UserContext } from '../UserContext';
 
 const UserLoginPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const history = useHistory();
+  const { login } = useContext(UserContext);
 
-  const handleSubmit = (event) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [newUsername, setNewUsername] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+
+  const handleLogin = (event) => {
     event.preventDefault();
 
-    // Lógica de autenticação do usuário aqui
-    // ...
+    // Realize a validação dos dados de login
+    if (username.trim() !== '' && password.trim() !== '') {
+      const userData = {
+        username,
+        password,
+      };
 
-    // Limpar campos de entrada após o envio
-    setEmail('');
-    setPassword('');
+      login(userData);
+      history.push('/');
+    } else {
+      alert('Por favor, preencha todos os campos');
+    }
+  };
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleNewUsernameChange = (event) => {
+    setNewUsername(event.target.value);
+  };
+
+  const handleNewPasswordChange = (event) => {
+    setNewPassword(event.target.value);
+  };
+
+  const handleRegistration = (event) => {
+    event.preventDefault();
+
+    // Realize a validação dos dados de cadastro
+    if (newUsername.trim() !== '' && newPassword.trim() !== '') {
+      // Adicione a lógica para salvar os dados do novo usuário no backend
+      // Por exemplo, você pode fazer uma requisição POST para uma rota de cadastro no seu backend
+
+      // Em seguida, redirecione o usuário para a página de login
+      history.push('/login/user');
+    } else {
+      alert('Por favor, preencha todos os campos');
+    }
   };
 
   return (
     <div>
       <h2>Login de Usuário</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleLogin}>
         <div>
           <label>
-            Email:
-            <input
-              type="email"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-              required
-            />
+            Usuário:
+            <input type="text" value={username} onChange={handleUsernameChange} />
           </label>
         </div>
         <div>
           <label>
             Senha:
-            <input
-              type="password"
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-              required
-            />
+            <input type="password" value={password} onChange={handlePasswordChange} />
           </label>
         </div>
         <button type="submit">Entrar</button>
+      </form>
+
+      <h2>Cadastre-se</h2>
+      <form onSubmit={handleRegistration}>
+        <div>
+          <label>
+            Novo Usuário:
+            <input type="text" value={newUsername} onChange={handleNewUsernameChange} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Nova Senha:
+            <input type="password" value={newPassword} onChange={handleNewPasswordChange} />
+          </label>
+        </div>
+        <button type="submit">Cadastrar</button>
       </form>
     </div>
   );
